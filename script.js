90d0ca655fd0c9f24a0f876a66f3a91e95f5582c
geraldaddey <geraldaddey@yahoo.com>
2022-03-30 14:48:39 +0000

    function created for displaying questions

--- script.js
+++ script.js
@@ -40,3 +40,36 @@ const quizData = [
     correct: "b",
   },
 ];
+
+const questionElement = document.getElementById("question");
+
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById("submit");
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+
+  questionElement.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+
+  //   console.log(currentQuizData.question, currentQuizData.a, currentQuizData.b);
+}
+
+submitButton.addEventListener("click", () => {
+  currentQuiz++;
+
+  if (currentQuiz < quizData.length) {
+    loadQuiz();
+  } else {
+  }
+});
