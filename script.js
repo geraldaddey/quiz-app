7fd1343ca46df51f1a993e4a1212d1d389da1f99
geraldaddey <geraldaddey@yahoo.com>
2022-04-01 08:40:03 +0000

    deselect function and score count added

--- script.js
+++ script.js
@@ -41,36 +41,69 @@ const quizData = [
   },
 ];
 
-const questionElement = document.getElementById("question");
-
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-const submitButton = document.getElementById("submit");
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+  deselectAnswers();
+
   const currentQuizData = quizData[currentQuiz];
 
-  questionElement.innerText = currentQuizData.question;
+  questionEl.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
-
-  //   console.log(currentQuizData.question, currentQuizData.a, currentQuizData.b);
 }
 
-submitButton.addEventListener("click", () => {
-  currentQuiz++;
+function getSelected() {
+  let answer = undefined;
 
-  if (currentQuiz < quizData.length) {
-    loadQuiz();
-  } else {
-    alert("congrats, you have completed the first part of this app.");
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+submitBtn.addEventListener("click", () => {
+  // check to see the answer
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      // alert("Quiz completed");
+      quiz.innerHTML = `
+                <h2 style = "padding: 1rem; text-align: center;"> You answered ${score}/${quizData.length} questions correctly.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+    }
   }
 });
